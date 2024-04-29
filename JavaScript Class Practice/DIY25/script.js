b156a436eada99567c21cf349053eb6313a64135
NichitaMladin <nichitamladin@gmail.com>
2024-04-29 20:20:50 +0300

    Initial commit

---
+++ JavaScript Class Practice/DIY25/script.js
@@ -0,0 +1,17 @@
+function maxBetween3Numbers(n1, n2, n3){
+    return Math.max(n1, n2, n3);
+}
+
+console.log(maxBetween3Numbers(12, 18, 3));
+
+function findMax(){
+    let num1 = parseInt(prompt("Add first number:"));
+    let num2 = parseInt(prompt("Add the second number: "));
+    let num3 = parseInt(prompt("Add the third number: "));
+
+    let maxNumber = maxBetween3Numbers(num1, num2, num3);
+
+    let maxParagraph = document.getElementById("max");
+    maxParagraph.textContent = "Max number: " + maxNumber;
+
+}
